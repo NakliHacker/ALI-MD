6f95d8a83af742d9a2e38a09e56fb2f499fe0045
-𝐀ɭīī 𝐈𝐍𝅦𝐗īī𝐃𝐄^᪲᪲᪲ 💀🎀 <alirazalashari457@gmail.com>
2025-11-09 14:12:56 +0500

    ALI-MD ❤️‍🩹

--- config.js
+++ config.js
@@ -1,48 +1,51 @@
-const fs = require('fs'),   
-      dotenv = fs.existsSync('config.env') ? require('dotenv').config({ path: '/.env' }) : undefined,  
-      convertToBool = (text, fault = 'true') => text === fault;  
-  
-global.session = "https://ali-pair-xode.onrender.com";   
-   
-module.exports = {  
-SESSION_ID: process.env.SESSION_ID || "ALI-MD~hjVKWGwbZ+4wBpX3s4gIePwCUQlDafwcAAA==", // Add sess Id here espwcially when deploying on panels else use app.json and .env file...  
-SUDO_NUMBERS: process.env.SUDO_NUMBERS || "", //Add multiple Numbers with Country Codes without (+) Separated by Comma...  
-ANTI_DELETE: process.env.ANTI_DELETE || "true", // ✅ only ONE, default = true  
-AUTO_STATUS_VIEW: process.env.AUTO_STATUS_VIEW || "true",  
-AUTO_STATUS_REACTS: process.env.AUTO_STATUS_REACTS || "true",  
-STATUS_REACTION: process.env.STATUS_REACTION || "💛,❤️,💜,🤍,💙", //Input Yours Custom...Can be one Emoji or Multiple Emojis Separated by Commas  
-AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",  
-STATUS_REPLY_MSG: process.env.STATUS_REPLY_MSG || "✅️ Status Viewed by ALI-MD", // // Input Yours custom...  
-MODE: process.env.MODE || "public", // Put private or public or inbox or groups  
-OWNER_NUMBER: process.env.OWNER_NUMBER || "923197521693", // Only 1 owner Number Here, others Add to sudo numbers...  
-OWNER_NAME: process.env.OWNER_NAME || "𓂃ᷱ᪳𝐀ɭīī 𝐈𝐍𝅦𝐗īī𝐃𝐄^᪲᪲᪲⤸֓同", // Input Yours custom...(Maintain font for Flow)  
-PACK_AUTHOR: process.env.PACK_AUTHOR || "", // Added // Input Yours custom...  
-PACK_NAME: process.env.PACK_NAME || "",
-HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
-HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",// Added // Input Yours custom...  
-PREFIX: process.env.PREFIX || ".",  
-VERSION: process.env.VERSION || "6.5.0",  
-ANTILINK: process.env.ANTILINK || "false",//  Enter true to kick automatically or delete to delete without kicking or warn to warn before kicking  
-ANTICALL: process.env.ANTICALL || "false",  
-MENTION_REPLY: process.env.MENTION_REPLY || "false",
-ANTIBAD: process.env.ANTIBAD || "false",
-BAD_WORDS: process.env.BAD_WORDS || "fuck, pussy, anus, idiot", // Add Yours Separated by Comma(will be deleted if ANTIBAD is set to true)
-ANTICALL_MSG: process.env.ANTICALL_MSG || "*📞 ᴄαℓℓ ɴσт αℓℓσωє∂ ιɴ тнιѕ ɴᴜмвєʀ уσυ ∂σɴт нανє ᴘєʀмιѕѕισɴ 📵*",  
-AUTO_REACT: process.env.AUTO_REACT || "false",  
-OWNER_REACT: process.env.OWNER_REACT || "false",  
-BOT_NAME: process.env.BOT_NAME || "𓆩ု᪳𝐀ɭīī 𝐌𝐃ှ᪳𓆪", //  don't change   
-BOT_PIC: process.env.BOT_PIC || "https://files.catbox.moe/6ku0eo.jpg", //  don't change   
-AUTO_AUDIO: process.env.AUTO_AUDIO || "false",  
-AUTO_BIO: process.env.AUTO_BIO || "false",  
-AUTO_BIO_QUOTE: process.env.AUTO_BIO_QUOTE || "ALI MD ALIVE",  
-WELCOME: process.env.WELCOME || "false",  
-//not working for the moment do don't on it  
-AUTO_READ_MESSAGES: process.env.AUTO_READ_MESSAGES || "false", // Enter value to true for blueticking all messages, or commands for blueticking only commands else false  
-AUTO_BLOCK: process.env.AUTO_BLOCK || "333,799", // Add Multiple Country Codes Separated by Comma...  
-PRESENCE: process.env.PRESENCE || "null", // Choose one: typing, recording, online, null  
-TIME_ZONE: process.env.TIME_ZONE || "Asia/Karachi", // Enter yours else leave blank if not sure  
-};  
-  
-let file = require.resolve(__filename);   
-fs.watchFile(file, () => { fs.unwatchFile(file); console.log(`Update '${__filename}'`); delete require.cache[file]; require(file); });  
-// That's All...
+const fs = require('fs'),
+      dotenv = fs.existsSync('config.env') ? require('dotenv').config({ path: '/.env' }) : undefined,
+      convertToBool = (text, fault = 'true') => text === fault;
+
+global.session = "https://ali-pair-xode.onrender.com";
+
+module.exports = {
+    SESSION_ID: process.env.SESSION_ID || "ALI-MD~hjVKWGwbZ+4wBpX3s4gIePwCUQlDafwcAAA==", // Add sess Id here especially when deploying on panels else use app.json and .env file...
+    SUDO_NUMBERS: process.env.SUDO_NUMBERS || "", // Add multiple numbers with country codes without (+), separated by comma
+    ANTI_DELETE: process.env.ANTI_DELETE || "true", // ✅ only ONE, default = true
+    AUTO_STATUS_VIEWS: process.env.AUTO_STATUS_VIEWS || "true",
+    AUTO_STATUS_REACTS: process.env.AUTO_STATUS_REACTS || "true",
+    AUTO_STATUS_EMOJIS: process.env.AUTO_STATUS_EMOJIS || "❤️,💀,🌚,🌟,🔥,❤️‍🩹,🌸,🍁,🍂,🦋,🍥,🍧,🍨,🍫,🍭,🎀,🎐,🎗️,👑,🚩,🇵🇰,🍓,🍇,🧃,🗿,🎋,💸,🧸,🦢,✨,🌾,🌊,⚡,🌏,🕸️,🎀,🪄,🌝,🌜,💫,🤍,🖤,🤎,💜,💙", // Input your custom emojis
+    AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
+    STATUS_REPLY_MSG: process.env.STATUS_REPLY_MSG || "✅️ Status Viewed by ALI-MD", // Custom message
+    MODE: process.env.MODE || "public", // Options: private, public, inbox, groups
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "923197521693", // Only 1 owner number here, others add to sudo numbers
+    OWNER_NAME: process.env.OWNER_NAME || "𓂃ᷱ᪳𝐀ɭīī 𝐈𝐍𝅦𝐗īī𝐃𝐄^᪲᪲᪲輪", // Custom name
+    PACK_AUTHOR: process.env.PACK_AUTHOR || "", // Custom
+    PACK_NAME: process.env.PACK_NAME || "",
+    HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
+    HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "", // Custom
+    PREFIX: process.env.PREFIX || ".",
+    VERSION: process.env.VERSION || "7.0.0",
+    ANTI_LINK: process.env.ANTI_LINK || "false", // true = kick, delete = delete, warn = warn
+    ANTI_CALL: process.env.ANTI_CALL || "false",
+    ANTIBAD: process.env.ANTIBAD || "false",
+    BAD_WORDS: process.env.BAD_WORDS || "fuck, pussy, anus, idiot", // Will be deleted if ANTIBAD is true
+    ANTICALL_MSG: process.env.ANTICALL_MSG || "*📞 ᴄαℓℓ ɴσт αℓℓσωє∂ ιɴ тнιѕ ɴᴜмвєʀ уσυ ∂σɴт нανє ᴘєʀмιѕѕισɴ 📵*",
+    AUTO_REACT: process.env.AUTO_REACT || "false",
+    OWNER_REACT: process.env.OWNER_REACT || "false",
+    BOT_NAME: process.env.BOT_NAME || "𓆩ု᪳𝐀ɭīī 𝐌Ɗှ᪳𓆪", // Don't change
+    BOT_PIC: process.env.BOT_PIC || "https://files.catbox.moe/2ka956.jpg", // Don't change
+    AUTO_AUDIO: process.env.AUTO_AUDIO || "false",
+    AUTO_BIO: process.env.AUTO_BIO || "false",
+    AUTO_BIO_QUOTE: process.env.AUTO_BIO_QUOTE || "ALI MD ALIVE",
+    WELCOME: process.env.WELCOME || "false",
+    AUTO_READ_MESSAGES: process.env.AUTO_READ_MESSAGES || "false", // true = bluetick all messages, commands = bluetick commands only
+    AUTO_BLOCK: process.env.AUTO_BLOCK || "333,799", // Multiple country codes separated by comma
+    PRESENCE: process.env.PRESENCE || "null", // typing, recording, online, null
+    TIME_ZONE: process.env.TIME_ZONE || "Asia/Karachi", // Enter your timezone
+};
+
+// Auto-reload this config when file changes
+let file = require.resolve(__filename);
+fs.watchFile(file, () => {
+    fs.unwatchFile(file);
+    console.log(`Update '${__filename}'`);
+    delete require.cache[file];
+    require(file);
+});
